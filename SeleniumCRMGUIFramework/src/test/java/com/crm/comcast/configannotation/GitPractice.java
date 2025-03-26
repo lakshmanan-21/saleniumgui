dc0f480eb9499a48ab7c9d284a98bd2baee86cfc
Admin <Admin@DESKTOP-QHFD0PL>
2025-03-26 12:18:09 +0530

    added new statement

--- SeleniumCRMGUIFramework/src/test/java/com/crm/comcast/configannotation/GitPractice.java
+++ SeleniumCRMGUIFramework/src/test/java/com/crm/comcast/configannotation/GitPractice.java
@@ -5,6 +5,7 @@ public class GitPractice
 public static void main(String[] args) {
 	System.out.println("Framework in github");
 	System.out.println("mokshith");
+	System.out.println("statement1");
 }
 
 }
